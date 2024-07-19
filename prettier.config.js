583ddb01d5e600d8d2722c7146d29c3dbec758a9
Sergio Majé <smajefranco@gmail.com>
2024-07-19 11:47:55 -0500

    🔧 Add ESLint and Prettier configuration

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 90,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  // plugins: ['prettier-plugin-tailwindcss'],
+  // tailwindConfig: './tailwind.config.js',
+  // tailwindFunctions: ['clsx', 'twMerge', 'cn']
+};
+
+module.exports = config;
