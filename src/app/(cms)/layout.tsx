a8957570d80548799aa6670662d09a9cc3642836
Sergio Majé <smajefranco@gmail.com>
2024-07-20 10:03:39 -0500

    🚧 Define CMS and root main pages and layout

---
+++ src/app/(cms)/layout.tsx
@@ -0,0 +1,23 @@
+import '../globals.css';
+
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Medical CMS App',
+  description: 'Medical app',
+};
+
+export default function CmsRootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
