bc65905e5e893e23c3551e1d01677a7983e5e4ad
Sergio Majé <smajefranco@gmail.com>
2024-07-26 12:54:42 -0500

    🔧 Set image domains in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.tailgrids.com',
+        port: '',
+        pathname: '/2.0/image/assets/images/brands/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.tailgrids.com',
+        port: '',
+        pathname: '/1.0/assets/images/hero/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
