2917938a263ae24ef2e1f42f3a4b5412eff21469
Sergio Majé <smajefranco@gmail.com>
2024-08-15 19:18:07 -0500

    💄 Improve colors for hero component

--- src/app/(public)/(home)/hero.tsx
+++ src/app/(public)/(home)/hero.tsx
@@ -3,21 +3,6 @@ import Image from 'next/image';
 
 import { cn } from '@/lib/utils';
 
-const SingleImage: React.FC<{
-  href: string;
-  imgSrc: string;
-}> = ({ href, imgSrc }) => (
-  <a href={href} className='flex w-full items-center justify-center'>
-    <Image
-      src={imgSrc}
-      alt='brand image'
-      width={110}
-      height={30}
-      className='h-10 w-full'
-    />
-  </a>
-);
-
 export default function Hero() {
   return (
     <header
@@ -32,8 +17,7 @@ export default function Hero() {
         'justify-center',
         'gap-10',
         'box-border',
-        'bg-white',
-        'dark:bg-blue-950',
+        'bg-blue-950 text-gray-50',
       )}
     >
       <section className='flex flex-col md:max-w-[40vw] lg:pl-16'>
@@ -43,15 +27,13 @@ export default function Hero() {
             'text-4xl',
             'lg:text-5xl',
             'font-bold',
-            'text-blue-950',
-            'dark:text-gray-50',
             'leading-tight',
             'text-pretty',
           )}
         >
           The Greatest Journey Of Online Payment.
         </h1>
-        <p className='mb-8 text-balance text-base text-blue-950 dark:text-gray-50'>
+        <p className='mb-8 text-balance text-base'>
           With TailGrids, business and students thrive together. Business can perfectly
           match their staffing to changing demand throughout the dayed.
         </p>
@@ -64,14 +46,14 @@ export default function Hero() {
                 'items-center',
                 'justify-center',
                 'rounded-md',
-                'bg-blue-700',
+                'bg-blue-500',
                 'px-6',
                 'py-3',
                 'text-center',
                 'text-base',
                 'font-medium',
                 'text-gray-50',
-                'hover:bg-blue-900',
+                'hover:bg-blue-700',
                 'lg:px-7',
               )}
             >
@@ -91,55 +73,26 @@ export default function Hero() {
                 'text-center',
                 'text-base',
                 'font-medium',
-                'text-blue-950',
-                'hover:text-blue-700',
-                'dark:text-gray-50',
+                'hover:text-blue-500',
               )}
             >
-              <CircleArrowDown className='text-blue-700' />
+              <CircleArrowDown />
               <span>Download App</span>
             </a>
           </li>
         </ul>
-        <section
-          className={'grid w-3/4 grid-cols-3 grid-rows-2 items-center gap-x-4 pt-16'}
-        >
-          <h6
-            className={cn(
-              'col-span-full',
-              'mb-6',
-              'flex',
-              'items-center',
-              'text-xs',
-              'font-normal',
-              'text-blue-950',
-              'dark:text-gray-50',
-            )}
-          >
-            Some Of Our Clients{' '}
-            <span className='ml-3 inline-block h-px w-8 bg-blue-950'></span>
-          </h6>
-          <SingleImage
-            href='/#'
-            imgSrc='https://cdn.tailgrids.com/2.0/image/assets/images/brands/ayroui.svg'
-          />
-          <SingleImage
-            href='/#'
-            // eslint-disable-next-line max-len
-            imgSrc='https://cdn.tailgrids.com/2.0/image/assets/images/brands/graygrids.svg'
-          />
-          <SingleImage
-            href='/#'
-            imgSrc='https://cdn.tailgrids.com/2.0/image/assets/images/brands/uideck.svg'
-          />
-        </section>
       </section>
       <section className='mx-auto mb-6 box-border flex w-full px-4 lg:w-6/12'>
         <div className='relative z-10 inline-block pt-8 lg:pt-0'>
           <Image
             src='https://cdn.tailgrids.com/1.0/assets/images/hero/hero-image-01.png'
             alt='hero'
-            className='max-w-full md:max-w-[40vw] lg:ml-auto'
+            className={cn(
+              'max-w-full',
+              'drop-shadow-[0_1px_2px_rgb(from_white_r_g_b_/_0.5)]',
+              'md:max-w-[40vw]',
+              'lg:ml-auto',
+            )}
             width={500}
             height={650}
           />
